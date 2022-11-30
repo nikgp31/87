7a1c53e7de878ed85713dc4fa1c9d60d2f83166e
nikgp31 <106701320+nikgp31@users.noreply.github.com>
2022-11-30 18:07:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+
+var canvas = new fabric.Canvas('myCanvas');
+ blockY=1;
+ blockX=1;
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+function newImage(getImage)
+{
+	fabric.Image.fromURL(get_image, function (img)){
+block_image_object = Img;
+
+block_image_object.scaleToWidht(block_image_widht);
+block_image_object.scaleToHeight(block_image_Height);
+block_image_object.set({
+top:block_y,
+left:block_x
+});
+canvas.add(block_image_object);
+	};
+	}
+
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // adicione os códigos adequados às teclas
+	{
+		new_image('rr1.png')
+		console.log("r");
+
+	if(keyPressed == '86')
+	{
+		blockX = 200;
+		new_image('rr1.png')
+		console.log("r");
+		
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		new_image('rr1.png')
+		console.log("r");
+		
+	}
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		new_image('rr1.png')
+		console.log("r");
+		
+	}
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+		new_image('rr1.png')
+		console.log("r");
+		
+	}
+	
+}
+
